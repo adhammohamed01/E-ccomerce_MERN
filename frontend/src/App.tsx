8309c27c1617be55f25e9ee85743ec0bfefca5b8
adhammohamed01 <adham.mhamed2001@gmail.com>
2025-07-23 03:26:06 +0300

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Homepage from "./pages/HomePage"
+import Navbar from "./components/Navbar"
 
 
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
    <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
          
